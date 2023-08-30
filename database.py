be86c9f1fe55fda9312e1929c404c53bdb9301f5
harel <harelleibovich@gmail.com>
2023-08-30 10:58:56 +0300

    update

---
+++ database.py
@@ -0,0 +1,4 @@
+def insert_database(Id, Age, Working_condition, FinalWeight,
+                    Education, YearsOfEducation, MaritalStatus, Occupation, Relationship, Gender,
+                    InvestmentGains, InvestmentLosses, WeeklyWorkHours, CountryOfOrigin):
+    pass
