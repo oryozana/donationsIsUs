d5e0c86446c85319339576f4e21e7266d6618a95
the12devil <65110625+the12devil@users.noreply.github.com>
2023-08-30 16:24:49 +0300

    Update test_client.py

--- test_client.py
+++ test_client.py
@@ -1,19 +1,31 @@
 # Import socket module
 import socket
 
+import Consts
+
 
 def Main():
-    host = '127.0.0.1'
+    host = '10.56.145.166'
+    # Define the port on which you want to connect
     port = 2005
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    while True:
-        message = input("enter_name")
-        s.send(message.encode())
-        data = s.recv(1024)
-        print('Received from the server :', str(data.decode('ascii')))
 
-    # close the connection
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # connect to server on local computer
+    s.connect((host, port))
+
+    # message you send to server
+
+    while True:
+        code = input(f"Enter code\n1. get user stat code\n999. exit")
+        s.send(code.encode())  # send code to server
+        if code == Consts.GET_USER_STAT_CODE:
+            # send data of wanted person
+            msg_to_server = input("enter user details")
+            s.send(msg_to_server.encode())
+            msg_from_server = s.recv(1024).decode()
+            print(msg_from_server)
+
     s.close()
 
 
